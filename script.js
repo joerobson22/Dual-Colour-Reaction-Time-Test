cb9b132a52d7ba3e04a01ad72088b35437ccb1c6
Joe Robson <joe_a_r@icloud.com>
2025-10-15 16:28:39 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,239 @@
+const experiment = {
+  times: [],
+  maxTrials: 10,
+  waitHnd: -1,
+  started: false,
+  ended: false,
+  stimulusWait: false,
+  circleColours : ["", ""],
+  stimulusShownAt: -1,
+  btnDisabled:false
+};
+
+const minWaitTime = 0.1;
+const maxWaitTime = 1.0;
+
+const btn = document.querySelector(".button-default");
+const stimuli = document.querySelectorAll(".circle");
+let circle1 = stimuli[0];
+let circle2 = stimuli[1];
+let circles = [circle1, circle2];
+
+const colours = ["red", "orange", "green", "blue", "violet"]
+
+const advanceTrial = function () {
+  resetStimuli();
+
+  experiment.stimulusShown = false;
+  if (experiment.times.length < experiment.maxTrials) {
+    //still need to run more trials
+    scheduleStimulus();
+  } else {
+    //experiment ended
+    endExperiment();
+  }
+};
+
+const endExperiment = function () {
+  console.info("INFO: Experiment ended. Await download of results");
+
+  experiment.ended = true;
+
+  //Update Button Styling
+  experiment.btnDisabled = false;
+  btn.classList.toggle("button-enabled");
+  btn.classList.toggle("button-disabled");
+  btn.textContent = "Download Data";
+};
+
+const scheduleStimulus = function () {
+  var waitTime = (Math.random() * maxWaitTime) + minWaitTime;
+
+  experiment.stimulusWait = true;
+  experiment.waitHnd = window.setTimeout(changeRandomCircleColour, waitTime * 1000); //setTimeout runs in milliseconds
+};
+
+const showStimulus = function () {
+  experiment.stimulusShownAt = Date.now();
+  console.info(
+    "INFO: Trial",
+    experiment.times.length,
+    ". Stimulus shown",
+    experiment.stimulusShownAt
+  );
+  experiment.stimulusWait = false;
+  experiment.stimulusShown = true;
+};
+
+//------------------------
+const getRandomColour = function(){
+  return colours[Math.floor(Math.random() * colours.length)];
+}
+
+const changeRandomCircleColour = function(){
+  changeCircleColour(Math.floor(Math.random() * circles.length), getRandomColour());
+}
+
+const changeCircleColour = function (circleNum, colour) {
+  console.log("change circle " , circleNum, " to ", colour)
+
+  circles[circleNum].classList.remove("inactive");
+  deleteColoursFromStimulus(circleNum);
+
+  circles[circleNum].classList.add(colour);
+
+  experiment.circleColours[circleNum] = colour;
+
+  if (experiment.circleColours[0] == experiment.circleColours[1]){
+    showStimulus();
+  }
+  else{
+    scheduleStimulus();
+  }
+};
+
+const deleteColoursFromStimulus = function(circleNum){
+  for(let i = 0; i < colours.length; i++){
+    circles[circleNum].classList.remove(colours[i]);
+  }
+}
+
+const resetStimuli = function(){
+  console.log("Reset both circles");
+
+  for(let i = 0; i < circles.length; i++){
+    for(let j = 0; j < colours.length; j++){
+      circles[i].classList.remove(colours[j]);
+    }
+    circles[i].classList.remove("style");
+    circles[i].classList.add("inactive");
+    experiment.circleColours = ["", ""];
+  }
+}
+//-----------------------
+
+const logReaction = function () {
+  let userReactedAt = Date.now();
+  console.info("INFO: User reaction captured.", userReactedAt);
+
+  let deltaTime = userReactedAt - experiment.stimulusShownAt;
+  experiment.times.push(deltaTime);
+  document.querySelector("#time").textContent = deltaTime + " ms";
+};
+
+const userReaction = function () {
+  if (!experiment.started) {
+    return;
+  } //prior to start of experiment, ignore
+  if (experiment.stimulusWait) {
+    return;
+  } //ignore false trigger reactions
+
+  if (experiment.stimulusShown) {
+    //stimulus is visible, capture
+    logReaction();
+    advanceTrial();
+  }
+};
+
+const startExperiment = function () {
+  console.info("INFO: Experiment Started");
+  stimulus.style = "background-color:'';";
+  document.querySelector("#instructions").style.display = "none";
+  experiment.started = true;
+
+  window.addEventListener("keypress", onKey); //add keylistener
+  advanceTrial();
+};
+
+const btnAction = function () {
+  console.debug("DBG:", "click");
+  if(experiment.btnDisabled) return;
+  if (!experiment.ended) {
+    experiment.btnDisabled = true;
+    btn.classList.toggle("button-enabled");
+    btn.classList.toggle("button-disabled");
+  }
+  if (!experiment.started) {
+    startExperiment();
+  } else {
+    if (experiment.ended) {
+      exportExperimentLog();
+      const stats = computeStatistics(experiment.times);
+      document.querySelector("#time").textContent = [
+        "Count:",
+        stats.cnt,
+        "Mean:",
+        stats.mean.toFixed(2),
+        "ms",
+        "SD:",
+        stats.sd.toFixed(2),
+        "ms",
+      ].join(" ");
+    } else {
+      console.log("DBG: Should this occur?");
+    }
+  }
+};
+
+const computeStatistics = function (timeArr) {
+  //to get mean, get sum of all trials and divide by number of trials m = sum(x)/cnt(x)
+  const sums = timeArr.reduce((acc, num) => acc + num, 0);
+  const meanDeltaTime = sums / timeArr.length;
+
+  //standard deviation is  sqrt(sum(x-mean)^2/cnt(x))
+  const squaredDiffs = timeArr.reduce(
+    (acc, num) => (num - meanDeltaTime) ** 2 + acc,
+    0
+  );
+  const standardDeviationTime = Math.sqrt(squaredDiffs / timeArr.length);
+
+  return {
+    sd: standardDeviationTime,
+    mean: meanDeltaTime,
+    cnt: timeArr.length,
+  };
+};
+
+const exportExperimentLog = function () {
+  let csvHeader = "pid,trial#,reactionTime (ms)\n";
+  let pid = Math.floor(Math.random() * 900000) + 100000;
+  let csvData = experiment.times
+    .map((time, idx) => [pid, idx, time].join(","))
+    .join("\n"); //map passes every record in the log array to the getCSVDataLine, we also need to include pid to all rows
+  exportData(csvHeader + csvData, "VisualReactionTestResults.csv");
+};
+
+const exportData = function (csvDataString, exportFileName) {
+  // Create a Blob with the CSV data
+  const blob = new Blob([csvDataString], { type: "text/csv" });
+
+  // Create a temporary link element
+  const a = document.createElement("a");
+  a.href = window.URL.createObjectURL(blob);
+  a.download = exportFileName;
+
+  // Trigger the download
+  document.body.appendChild(a);
+  a.style.display = "none";
+  a.click();
+
+  // Clean up
+  window.URL.revokeObjectURL(a.href);
+  document.body.removeChild(a);
+};
+
+const onKey = function (evt) {
+  if (evt == null) {
+    evt = window.event;
+  }
+  switch (evt.which || evt.charCode || evt.keyCode) {
+    case 32: //space
+      userReaction();
+      break;
+    default:
+      console.warn("WARN: Key:", evt, evt.which, evt.charCode, evt.keyCode);
+  }
+};
+
+btn.addEventListener("click", btnAction);
